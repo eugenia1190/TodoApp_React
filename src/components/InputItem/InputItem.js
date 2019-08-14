3c4bbdb812572cc28f7193a2ca269a21f65490da
Evgenia Petukhova <chet444@mail.ru>
2019-08-14 20:53:24 +0500

    add material UI component in the project

--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
 
-const InputItem = () => (<div>InputItem</div>);
+const InputItem = () => (<div>
+	<TextField
+    	id="standard-dense"
+    	label="Добавить задачу"
+    	margin="dense"
+    	fullWidth
+    />
+</div>);
 
 export default InputItem;
