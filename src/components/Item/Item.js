4e4176849911a2d4f14426eecc84b849956dcad5
Evgenia Petukhova <chet444@mail.ru>
2019-08-14 20:09:59 +0500

    remove unnecessary space

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -2,7 +2,7 @@ import React from 'react';
 import classnames from 'classnames';
 import styles from './Item.module.css'
 
-const Item = ({ value, isDone }) => (<span className= {
+const Item = ({ value, isDone }) => (<span className={
 	classnames({
 		[styles.item]: true,
 		[styles.done]: isDone
