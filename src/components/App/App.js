bb35d7e6ed1b7e67a45c65f2f2584cdc35a780ed
Evgenia Petukhova <chet444@mail.ru>
2019-09-04 17:35:41 +0500

    create state for component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,33 +5,34 @@ import Footer from '../Footer/Footer';
 import styles from './App.module.css';
 
 class App extends React.Component {
+	state ={
+		items: [
+			{
+				value: 'Написать новое приложение',
+				isDone: false
+			},
+			{
+				value: 'прописать props-ы',
+				isDone: true
+			},
+			{
+				value: 'доделать остальные дела',
+				isDone: false
+			}
+		]
+	};
+	
 	render () {
-		const items = [
-		{
-			value: 'Написать новое приложение',
-			isDone: false
-		},
-		{
-			value: 'прописать props-ы',
-			isDone: true
-		},
-		{
-			value: 'доделать остальные дела',
-			isDone: false
-		}
-		
-	];
-
-
-	return (
-	<div className={styles.wrap}>
-		<h1 className={styles.title}>Все мои задачи:</h1>
-		<InputItem />
-		<ItemList 
-			items= {items} 
-		/> 
-		<Footer count={3} />
-	</div>);
+		return (
+			<div className={styles.wrap}>
+				<h1 className={styles.title}>Все мои задачи:</h1>
+				<InputItem />
+				<ItemList 
+					items= {this.state.items} 
+				/> 
+				<Footer count={3} />
+			</div>
+		);
 	}
 }
 
