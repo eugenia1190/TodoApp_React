d19dca7cc3475e551809dea782b2b2dbdac21166
Evgenia Petukhova <chet444@mail.ru>
2019-12-29 01:10:02 +0500

    add request repositories from github in About

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,39 @@
 import React from 'react';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Octokit from '@octokit/rest';
 
-const About = () =>
-(<div>Обо мне</div>)
+const octokit = new Octokit();
+
+class About extends React.Component {
+	state = {
+		isLoading: true
+	}
+
+	componentDidMount() {
+		octokit.repos.listForUser({
+			username: 'eugenia1190'
+		}).then(({ data }) => {this.setState({
+			repoList: data,
+			isLoading: false
+		})});
+	}
+
+	render() {
+		const { isLoading, repoList } = this.state;
+
+		return (
+			<div>
+				<h1>{ isLoading  ? <CircularProgress /> : 'Обо мне' }</h1>
+				<div>{!isLoading && <ol>
+					{repoList.map(repo => (<li key={repo.id}>
+						{repo.name}
+					</li>))}
+				</ol>} 
+				</div>
+			</div>
+
+		);
+	}
+}
 
 export default About;
