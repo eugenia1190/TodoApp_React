9cb739317720622a33d58546dac30e252cba132b
Evgenia Petukhova <chet444@mail.ru>
2020-01-09 22:05:00 +0500

    add information about user from GitHub in About

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,30 +7,55 @@ const octokit = new Octokit();
 
 class About extends React.Component {
 	state = {
-		isLoading: true
+		isLoading: true,
 	}
 
 	componentDidMount() {
+		const user = 'eugenia1190';
+		const url = 'https://api.github.com/users/'+ user;
+
 		octokit.repos.listForUser({
-			username: 'eugenia1190'
+			username: user
 		}).then(({ data }) => {this.setState({
 			repoList: data,
 			isLoading: false
 		})});
+
+		fetch(url)
+			.then (res => res.json())
+			.then (json => {
+				if (json.message == 'Not Found') {
+					this.setState ({
+						notFound: true,
+						isLoading: false
+					})
+				} else {
+					this.setState({
+						bio: json.bio,
+						avatar: json.avatar_url
+					})
+				}});
 	}
 
 	render() {
-		const { isLoading, repoList } = this.state;
+		const { isLoading, repoList, bio, avatar, notFound } = this.state;
 
 		return (
 			<div>
-				<h1>{ isLoading  ? <CircularProgress /> : 'Обо мне' }</h1>
-				<div>{!isLoading &&
-					<ol>
-						{repoList.map(repo => (<li key={repo.id}><a href={repo.html_url}  className={styles.link}>
-							{repo.name}
-						</a></li>))}
-					</ol>} 
+				<h1 className={styles.title}>{ isLoading  ? <CircularProgress /> : 'Обо мне' }</h1>
+				<div>
+					{!isLoading && notFound && <div>Информация о пользователе не доступна</div>}
+					{!isLoading && !notFound &&
+					<div>
+						<img src={avatar} alt="avatar" className={styles.avatar} />
+						<p> {bio} </p>
+						<h2>Мои репозитории:</h2>
+						<ol>
+							{repoList.map(repo => (<li key={repo.id}><a href={repo.html_url}  className={styles.link}>
+								{repo.name}
+							</a></li>))}
+						</ol> 
+					</div>} 
 				</div>
 			</div>
 
