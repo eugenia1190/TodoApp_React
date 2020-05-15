0749cd4c82dccb22af789f2fada47daf2886b89d
Evgenia Petukhova <chet444@mail.ru>
2020-05-15 19:51:48 +0500

    add color for buttons when it clicked in App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import Todo from '../Todo/Todo';
 import About from '../About/About';
 import styles from './App.module.css';
+import classnames from 'classnames';
 import '../../fonts/fonts.css';
 
 
@@ -11,12 +12,30 @@ import '../../fonts/fonts.css';
 
 const App = () => {
 
+	const initialState = {
+		display: 'todo',
+	}
+
+	const [display, setDisplay] = useState(initialState.display);
+
 return(<Router>
 	<div className={styles.wrap}>
 		<div className={styles.wrapper}>
 			<div className={styles.sidebar}>
-				<Link to='/' className={styles.link}><button className={styles.btn}>Обо мне</button></Link>
-				<Link to='/todo' className={styles.link}><button className={styles.btn}>Мои задачи</button></Link>
+				<Link to='/' className={styles.link}><button 
+					className={ 
+						classnames({
+							[styles.btn] : true,
+							[styles.active] : (display === 'about'),
+						})}
+					onClick = {() => setDisplay('about')}>Обо мне</button></Link>
+				<Link to='/todo' className={styles.link}><button
+					className={
+						classnames({
+							[styles.btn] : true,
+							[styles.active] : (display === 'todo'),
+						})}
+					onClick={() => setDisplay('todo')}>Мои задачи</button></Link>
 			</div>
 			<div>
 				<Route path='/' exact component={About} />
