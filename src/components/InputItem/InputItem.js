0f9f7fa73814c8ff5f216a5a7d8f8c3d36d68c00
Evgenia Petukhova <chet444@mail.ru>
2020-05-17 03:21:07 +0500

    change a class component InputItem to a function component

--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -1,60 +1,57 @@
-import React from 'react';
+import React, {useState} from 'react';
 import classnames from 'classnames';
 import styles from './InputItem.module.css';
 
 import AddIcon from '../../img/add.png';
 import Tooltip from '../Tooltip/Tooltip.js';
 
-class InputItem extends React.Component {
-	state = {
+const InputItem = ({onClickAdd}) => {
+	const initialState = {
 		inputValue: '',
-		labelValue: "Добавить задачу",
+		labelValue: 'Добавить задачу',
 		noValue: false,
 		error: false,
 	};
 
-	onButtonClick = () => {
-		this.setState ({
-			inputValue: '',
-			error: false,
-		});
+	const [inputValue, setInputValue] = useState(initialState.inputValue);
+	const [labelValue, setLabelValue] = useState(initialState.labelValue);
+	const [noValue, setNoValue] = useState(initialState.setNoValue);
+	const [error, setError] = useState(initialState.setError);
 
-		if ( this.state.inputValue === ''){
-			this.setState ({
-				labelValue: "Вы не ввели значение!",
-				noValue: true
-			});
+
+	const onButtonClick = () => {
+		setInputValue('');
+		setError(false);
+
+		if ( inputValue === ''){
+			setLabelValue('Вы не ввели значение!');
+			setNoValue(true);
 		} else {
-			this.setState ({
-				labelValue: "Добавить задачу",
-				noValue: false
-			});
-
-			this.props.onClickAdd(this.state.inputValue) === 'error' &&	this.setState({error: true});
+			setLabelValue('Добавить задачу');
+			setNoValue(false);
+			onClickAdd(inputValue) === 'error' && setError(true);
 		}
 	}
 
-	onClickEnter = (event) => { if (event.keyCode === 13) {this.onButtonClick()} };
+	const onClickEnter = (event) => { if (event.keyCode === 13) {onButtonClick()} };
 
-	render () {
-
-		return (
-			<div className = {styles.wrap}>
-				{this.state.error === true && <Tooltip />}
-				<input 
-					className = {
-						classnames({
-							[styles.input]: true,
-							[styles.error]: this.state.noValue || this.state.error,
-						})}
-					placeholder={this.state.labelValue}
-					value={this.state.inputValue}
-					onChange={event => this.setState({ inputValue: event.target.value, noValue: false, error: false })}
-					onKeyUp={this.onClickEnter}/>
-				<div><img src = {AddIcon} alt = 'add' className={styles.addicon} onClick={ this.onButtonClick } /></div>
-			</div>
-		);
-	}
+	return (
+		<div className = {styles.wrap}>
+			{error === true && <Tooltip />}
+			<input 
+				className = {
+					classnames({
+						[styles.input]: true,
+						[styles.error]: noValue || error,
+					})}
+				placeholder={labelValue}
+				value={inputValue}
+				onChange={event => { setInputValue(event.target.value); setNoValue(false); setError(false)}}
+				onKeyUp={onClickEnter}/>
+			<div><img src = {AddIcon} alt = 'add' className={styles.addicon} onClick={ onButtonClick } /></div>
+		</div>
+	)
+	
 }
 
 export default InputItem;
