aa520159352d9f123a76c0a6ad293a56c1bef421
Evgenia Petukhova <chet444@mail.ru>
2020-05-18 22:35:53 +0500

    add NavLink component and fix styles for links in App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,10 @@
-import React, {useState} from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 
 import Todo from '../Todo/Todo';
 import About from '../About/About';
 import Footer from '../Footer/Footer';
 import styles from './App.module.css';
-import classnames from 'classnames';
 import '../../fonts/fonts.css';
 
 
@@ -13,30 +12,16 @@ import '../../fonts/fonts.css';
 
 const App = () => {
 
-	const initialState = {
-		display: 'todo',
-	}
-
-	const [display, setDisplay] = useState(initialState.display);
-
 return(<Router>
 	<div className={styles.wrap}>
-		<div className={styles.sidebar}>
-			<Link to='/' className={styles.link}><button 
-				className={ 
-					classnames({
-						[styles.btn] : true,
-						[styles.btnActive] : (display === 'about'),
-					})}
-				onClick = {() => setDisplay('about')}>Обо мне</button></Link>
-			<Link to='/todo' className={styles.link}><button
-				className={
-					classnames({
-						[styles.btn] : true,
-						[styles.btnActive] : (display === 'todo'),
-					})}
-				onClick={() => setDisplay('todo')}>Мои задачи</button></Link>
-		</div>
+		<nav className={styles.sidebar}>
+			<NavLink exact to='/'
+				className={styles.link}
+				activeClassName={styles.linkActive}>Обо мне</NavLink>
+			<NavLink to='/todo'
+				className={styles.link}
+				activeClassName={styles.linkActive}>Мои задачи</NavLink>
+		</nav>
 		<div className={styles.content}>
 			<Route path='/' exact component={About} />
 			<Route path='/todo' component={Todo} />
